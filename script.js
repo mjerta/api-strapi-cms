89bf7369304fac10706524d97485a163d234435f
Maarten Postma <maarten.postma1000@gmail.com>
2023-09-21 15:15:37 +0200

    in progress

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 const url = "https://cms.mpdev.nl/api/projects-cards/";
-// const url = "http://example.com/movies.json";
 
 async function fetchData() {
   const response = await fetch(url);
   if (!response.ok) {
-    console.log("its not okay");
-    return response;
+    console.log(response)
+    throw { status: response.status, message: 'Server responded with an error status' };
   }
   const data = await response.json();
   return data;
 }
 
+function processData(callback) {
+const arr = callback.data;
+
+arr.forEach(element => {
+
+  const body = document.querySelector("body");
+  const card = document.createElement('div');
+  card.classList.add("cards");
+  body.appendChild(card);
+  const title = document.createElement('h1');
+  title.innerText = element.attributes.Title;
+  card.appendChild(title);
+  const excerpt = document.createElement('p');
+  excerpt.classList.add("excerpt")
+  excerpt.innerHTML = element.attributes.Description;
+  card.appendChild(excerpt);
+  console.log(element.attributes.activity)
+  
+});
+}
+
+
 fetchData()
-  .then((response) => {
-    console.log(response);
-  })
+  .then(processData)
   .catch((error) => {
     console.log(error);
   });
